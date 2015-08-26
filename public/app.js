4ce74c22648776e677d9142d637f30f5700bcf3a
Kate <katemcdonald39@gmail.com>
2015-08-26 11:52:07 -0400

    integrated angular into our app

---
+++ public/app.js
@@ -0,0 +1,11 @@
+var app = angular.module("NoteCards", []);
+app.controller("FlashCardController", function($scope) {
+	$scope.flashCard = {
+		question: 'What is Angular?',
+		answers: [
+			{ text: 'A front-end framework for great power!', correct: true },
+			{ text: 'Something lame, who cares, whatever.', correct: false },
+			{ text: 'Some kind of fish, right?', correct: false }
+		]
+	};
+});
